dd7b0b3ff50b314d27e0244342f82447f640dce8
Ítalo Kohlrausch <italokohlrausch@gmail.com>
2026-08-10 08:29:09 -0300

    ExMedia3

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	int a,b,c;
+	float d;
+	
+	a = 4;
+	b = 8;
+	c = 3;
+	d = (a+b+c)/3;
+	
+	printf("A media aritimetica dos valores %d, %d, %d eh %f", a,b,c,d);
+	
+	
+	return 0;
+}
