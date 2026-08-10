b2592a3b98da233ad24e77d9a7e462fc925b3c39
Ítalo Kohlrausch <italokohlrausch@gmail.com>
2026-08-10 08:46:14 -0300

    Add files via upload

---
+++ circulo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+
+	int raio;
+	float area;
+	
+	raio = 4;
+
+	area = 3.14*2*raio;
+	
+	printf("A area do circulo de raio %d eh %f", raio,area);
+	
+	
+	return 0;
+}
